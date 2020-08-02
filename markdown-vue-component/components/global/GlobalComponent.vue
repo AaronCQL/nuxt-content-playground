23e49b231615b48f474f200334c1047d8608046f
Aaron Choo <choozy.aaron@gmail.com>
2020-08-02 14:50:06 +0800

    feat: rework dir structure; add template

---
+++ markdown-vue-component/components/global/GlobalComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="background-color: azure;">
+    <h4>Hello from GlobalComponent</h4>
+    <ul>
+      <li>
+        <code>kebab-case-prop: {{ kebabCaseProp }}</code>
+      </li>
+      <li>
+        <code>camelCaseProp: {{ camelCaseProp }}</code>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const notSet = "Not set! Remember to use kebab-case prop names in the html/md.";
+
+export default {
+  name: "MyComponent",
+  props: {
+    kebabCaseProp: {
+      type: String,
+      default: notSet,
+    },
+    camelCaseProp: {
+      type: String,
+      default: notSet,
+    },
+  },
+};
+</script>
---
+++ markdown-vue-component/content/index.md
@@ -0,0 +1,9 @@
+# Index Page
+
+Hello from the **Index Page**.
+
+```html
+<global-component kebab-case-prop="Works!" camelCaseProp="Works!"></global-component>
+```
+
+<global-component kebab-case-prop="Works!" camelCaseProp="Works!"></global-component>
